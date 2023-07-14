1fe421864e556a2070f1d8e5d409244db5c3d08a
gwongibeom <k201gun@kakao.com>
2023-07-14 13:33:34 +0900

    에러를 넘어 에러를 넘어 에러를 넘어 에러를 넘어 에러를 넘어 에러를 넘어

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const app = express()
 app.use('/static', express.static('static'))
 
 app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/src/index.html')
+    res.sendFile(__dirname+'/index.html')
 })
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname+'/src/404.html')
+    res.status(404).sendFile(__dirname+'/404.html')
 });
 
 
