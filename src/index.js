6acbdc60c4394e520b000df8be1f724c4b1d8a73
gwongibeom <k201gun@kakao.com>
2023-07-14 11:05:50 +0900

    feat 2.2.0 add custom 404 page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+
+
 app.use(express.static('static'))
+
+app.get('/',(req,res) => {
+    return res.sendFile(__dirname+'/index.html')
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname+'/404.html')
+});
+
+
 app.listen(4000, () => {
     console.log(`ride on port 4000`);
 })
