d7134e61279f337c4635e32f5a23e5147048ab5a
gwongibeom <k201gun@kakao.com>
2023-07-14 12:44:46 +0900

    hotfix edit file path

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const app = express()
 app.use('/', express.static('static'))
 
 app.get('/',(req,res) => {
-    res.sendFile('./src/index.html')
+    res.sendFile(__dirname+'/index.html')
 })
 
 app.use((req, res, next) => {
-    res.status(404).sendFile('/404.html')
+    res.status(404).sendFile(__dirname+'/404.html')
 });
 
 
