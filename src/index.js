52ea77f19facc588fc69fa5f271000d67cc2f5b2
gwongibeom <k201gun@kakao.com>
2023-07-23 23:18:54 +0900

    edit frontend path

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
 
-
 app.use('/static', express.static('static'))
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/index.html')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(__dirname+'/404.html')
-});
-
+    res.status(404).sendFile(__dirname + '/404.html')
+})
 
 app.listen(4000, () => {
-    console.log(`ride on port 4000`);
+    console.log(`ride on port 4000`)
 })
