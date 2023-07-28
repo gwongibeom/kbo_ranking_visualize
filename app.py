ecb5e73fe24bff336dfa37a39fe0c398ee43038b
gwongibeom <k201gun@kakao.com>
2023-07-28 22:56:09 +0900

    edit css and watermark pos

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ for i, (logo, value) in enumerate(zip(team_logos, games_behind_values)):
     ax.add_artist(ab)
     ax.get_children()[i].set_color(colors.get(team_names[i], "blue"))
 
-ax.text(0.5, 0.5, f'kbo.gwon.us \n {today} {current_time}', transform=ax.transAxes,
+ax.text(0, 0, f'kbo.gwon.us \n {today} {current_time}', transform=ax.transAxes,
         fontsize=20, color='gray', alpha=0.7,
         ha='center', va='center', rotation=34,
         font=fpath)
