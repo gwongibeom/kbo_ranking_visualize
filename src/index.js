29b600f5989ec4c7a999eadd1705fa441e063d0e
gwongibeom <k201gun@kakao.com>
2023-08-16 23:49:49 +0900

    Revert "add 404 auto route"
    This reverts commit c6f3faa26ba3a7ea22911cc285fde863d2334174.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
-const express = require('express');
-const fs = require('fs/promises');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.set('view engine', 'ejs');
 
-app.use('/static', express.static('static'));
+app.use('/static', express.static('static'))
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-});
+    res.sendFile(__dirname + '/index.html')
+})
 
 app.get('/robots.txt', (req, res) => {
-    res.type("text/plain");
-    res.send(`User-agent: *\nAllow: /\n`);
-});
+    res.type("text/plain")
+    res.send(
+        `User-agent: *\nAllow: /\n`
+    )
+})
 
-app.get('/detail/:year/:month/:day', async (req, res) => {
-    const { year, month, day } = req.params;
-    const filename = `KBO${year}-${month}-${day}.png`;
-    const filePath = `${__dirname}/static/${filename}`;
-
-    try {
-        await fs.access(filePath);
-        res.render("detail", { year, month, day });
-    } catch (err) {
-        res.sendFile(`${__dirname}/404.html`);
-    }
-});
+app.get('/detail/:year/:month/:day', function (req, res) {
+    const paramsYear = req.params.year
+    const paramsMonth = req.params.month
+    const paramsDay = req.params.day
+    res.render("detail",{ year: paramsYear, month: paramsMonth, day: paramsDay})
+})
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(`${__dirname}/404.html`);
-});
+    res.status(404).sendFile(__dirname + '/404.html')
+})
 
 app.listen(4000, () => {
-    console.log(`🏄 ride on port 4000`);
-});
+    console.log(`🏄 ride on port 4000`)
+})
